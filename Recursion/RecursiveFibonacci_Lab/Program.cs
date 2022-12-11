4191eff5d3f8f42cdbb20716c99e61467cb90717
mayapeneva <maya.peneva@gmail.com>
2022-12-11 14:39:02 +0200

    Added a solution

---
+++ Recursion/RecursiveFibonacci_Lab/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace RecursiveFibonacci_Lab
+{
+    using System;
+
+    public class Program
+    {
+        private static int[] FibonacciNumbers;
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            FibonacciNumbers = new int[n];
+
+            var number = GetFibonacci(n);
+            Console.WriteLine(number);
+        }
+
+        private static int GetFibonacci(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            var firstIndex = n - 1;
+            var firstNumber = FibonacciNumbers[firstIndex];
+            if (firstNumber == default)
+            {
+                firstNumber = GetFibonacci(firstIndex);
+                FibonacciNumbers[firstIndex] = firstNumber;
+            }
+
+            var secondIndex = n - 2;
+            var secondNumber = FibonacciNumbers[secondIndex];
+            if (secondNumber == default)
+            {
+                secondNumber = GetFibonacci(secondIndex);
+                FibonacciNumbers[secondIndex] = secondNumber;
+            }
+
+            return firstNumber + secondNumber;
+        }
+    }
+}
